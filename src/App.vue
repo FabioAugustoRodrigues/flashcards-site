93f770288dcdfac427ae44bcc37dc32ae13b259c
FabioAugustoRodrigues <fabio.augusto.rodrig@gmail.com>
2025-04-25 22:13:41 -0300

    style: add app navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
-<template>
-  <router-link to="/" class="navbar-brand">Flashcards</router-link>
+<script setup>
+import { ref } from 'vue';
+import logoPath from './assets/owl-96.png';
 
-  <div class="container mt-4">
-    <router-view></router-view>
+const logo = ref(logoPath);
+</script>
+
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+      <div class="container">
+        <img :src="logo" alt="Logo" class="navbar-logo" />
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mt-4">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
+
+<style>
+.navbar-logo {
+  height: 35px;
+  margin-right: 10px;
+}
+</style>
