1e172359b4428ab4a586e7146fe89908c4dc77c1
FabioAugustoRodrigues <fabio.augusto.rodrig@gmail.com>
2025-05-02 15:45:16 -0300

    style: show clasification on cards table

--- src/components/CardTable.vue
+++ src/components/CardTable.vue
@@ -4,12 +4,18 @@
             <thead class="table-dark">
                 <tr>
                     <th>Question</th>
+                    <th>Class.</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="card in cards" :key="card.id">
-                    <td class="col-10">{{ card.question }}</td>
+                    <td class="col-9">{{ card.question }}</td>
+                    <td class="col-1">
+                        <span v-if="card.classification === 3" class="w-100 badge bg-success">Easy</span>
+                        <span v-else-if="card.classification === 2" class="w-100 badge bg-warning">Medium</span>
+                        <span v-else class="w-100 badge bg-danger">Hard</span>
+                    </td>
                     <td class="col-2">
                         <div class="row justify-content-center">
                             <div class="col-12 col-md-6 mb-2">
